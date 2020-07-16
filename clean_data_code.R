910f258299cb54261c1b06ecc459681037aa931d
ElenaVillalobos <villaele14@gmail.com>
2020-07-16 12:09:14 -0500

    comentarios en código

--- clean_data_code.R
+++ clean_data_code.R
@@ -1,46 +1,53 @@
+# Clean working directory
 rm(list=ls())
+
+# Packages
 library(tidyverse)
 
-eco_5 <- read.csv('2019-05.csv')
-dim(eco_5)
-head(eco_5)
-class(eco_4$Fecha_Hora_Retiro)
-eco_4$Fecha_Hora_Retiro[3]
+# Setting working directory
+setwd("~/Documents/Prope20MCD/Practicas_equipos/practica-final-por-equipos-verano-2020-itam-Eduardo-Moreno")
 
-eco_3 <- read.csv('2020-05.csv')
-dim(eco_3)
-head(eco_3)
+# Datos Mayo 19
+may_19 <- read.csv('2019-05.csv')
+dim(may_19)
+head(may_19)
+class(may_19$Fecha_Hora_Retiro)
+may_19$Fecha_Hora_Retiro[3]
 
-eco_3 <- arrange(eco_3,Fecha_Hora_Retiro)
+# Datos Mayo 20
+may_20 <- read.csv('2020-05.csv')
+dim(may_20)
+head(may_20)
 
-hora_retiro <- eco_3$Fecha_Hora_Retiro
+# Observando la base de mayo 20, esta ordenada de acuerdo al usuario
+# entonces la ordenamos de acuerdo a la fecha y hora
+may_20 <- arrange(may_20,Fecha_Hora_Retiro)
 
-otra <- tidyr::separate(eco_3, Fecha_Hora_Retiro, 
+# Otra observacion es que la hora y la fecha estan en una sola variable
+# por lo tanto la separamos
+may_20 <- tidyr::separate(may_20, Fecha_Hora_Retiro, 
                 c("Fecha_Retiro", "Hora_retiro"), 
                 sep = " ")
 
-otra <- tidyr::separate(otra, Fecha_Hora_Arribo, 
+may_20 <- tidyr::separate(may_20, Fecha_Hora_Arribo, 
                         c("Fecha_Arribo", "Hora_Arribo"), 
                         sep = " ")
 
-head(otra)
-dim(otra)
+# Observamos la base
+head(may_20)
+dim(may_20)
 
-
-base_bien <- otra[,2:10]
+# Creamos otra base sin tomar en cuenta el ID de usuario
+base_bien <- may_20[,2:10]
 head(base_bien)
 base_bien$Hora_Arribo[1]
 length(eco_5$Hora_Arribo)
 
-
-glimpse(eco_5)
+# Observamos la base
+glimpse(may_20)
 glimpse(base_bien)
 
+# La escribimos de nuevo
 write.csv(base_bien,file='Mayo20.csv',row.names = F)
 
-ot <- read.csv('Mayo20.csv')
-dim(ot)
-glimpse(ot)
-head(ot)
 
-arrange(ot,Fecha_Retiro)
